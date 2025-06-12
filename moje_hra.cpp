1019d6006ed701bef2f7e3c39efbe27340eea4e6
ZuzeV2 <zuzana.varadiova@skolakrizik.cz>
2025-06-12 21:46:30 +0200

    Vylepšení kombatu ohledně inventáře
    Přidání přehledu inventáře při souboji.

--- moje_hra.cpp
+++ moje_hra.cpp
@@ -25,8 +25,12 @@ bool GameOver(float &zzivoty, string Inventar[5], float mxzivoty) {
 
 // Vraci nahodny pocet enemy (1-3)
 // PUVODNE funkce nic nevracela, nyni vraci int
-void soubojSequence(float zzivoty, float mxzivoty, string Inventar[5], string Klasa, float Utok, float enemyZivoty[3], int enemyReturn){
+void soubojSequence(float zzivoty, float mxzivoty, string Inventar[5], string Klasa, float Utok, float enemyZivoty[3], int enemyReturn, float &mana, float maxmana){
     int akce;
+    int inventar2;
+    bool prazdny;
+//smycka pro opakovani vyberu
+while(true){
 do{
 cout << "Co chcete delat za akci?\n";
 cout << "1 - Utok\n";
@@ -36,7 +40,8 @@ cout << "4 - Preskocit svuj turn\n";
 cin >> akce;
 }while(!(akce == 1 || akce == 2 || akce == 3 || akce == 4));
 
-if(akce == 1){
+switch(akce){
+case 1:{
     int cil;
 
     do{
@@ -54,18 +59,98 @@ if(akce == 1){
      cout << "Utocil jsi nepritele " << cil + 1 << "a zpusobil jsi " << poskozeni << "poskozeni! Zbyle zivoty: " << enemyZivoty[cil] << "\n";
      if (enemyZivoty[cil] < 0){
         enemyZivoty[cil] = 0;
-        cout << "Nepritel" << cil + 1 << "zemrel\n";
+        cout << "Nepritel#" << cil + 1 << "zemrel\n";
+     }
+     return;
+}
+case 2:{
+    prazdny = true;
+    for (int i = 0; i < 5; i++) {
+        if (!Inventar[i].empty()) {
+            prazdny = false;
+            break;
+        }
+    }
+    if (prazdny) {
+        cout << "Tvuj inventar je prazdny. Neni co pouzit.\n";
+        continue;
+    }
+
+    cout << "Inventar: \n";
+    for (int i = 0; i <5; i++){
+        cout << i + 1 << " - " << Inventar[i] << "\n";
+    }
+    cout << "Chcete neco pouzit? (1 - 5 podle umisteni veci v inventari, 0 pro zruseni) \n";
+    cin >> inventar2;
+
+    if(inventar2 == 0){
+        cout << "Rozhodl jste se nic nepouzit\n";
+        continue;
+    }
+    inventar2--;
+
+     if (inventar2 < 0 || inventar2 >= 5 || Inventar[inventar2].empty()) {
+        cout << "Neplatna volba nebo prazdne misto.\n";
+        continue;
+    }
+    string item = Inventar[inventar2];
+
+    if (item == "Jablko") {
+        zzivoty += 3;
+        if (zzivoty > mxzivoty){
+                zzivoty = mxzivoty;}
+        cout << "Pouzil jsi jablko! Mas ted " << zzivoty << " HP.\n";
+        }
+        else if (item == "Susenka") {
+        zzivoty += 4;
+        if (zzivoty > mxzivoty){
+        zzivoty = mxzivoty;}
+        cout << "Snedl jsi susenku! Mas ted " << zzivoty << " HP.\n";
+    }
+    else if (item == "Alkohol") {
+        mana += 4;
+        if (mana > maxmana){
+        mana = maxmana;
+        }
+        cout << "Vypil jsi alkohol. Mana je ted " << mana << ".\n";
+    } else if (item == "Magicky potion") {
+        zzivoty += 5;
+        if (zzivoty > mxzivoty){
+            zzivoty = mxzivoty;
+        }
+        mana += 5;
+        if (mana > maxmana){
+            mana = maxmana;
+        }
+        cout << "Pouzil jsi magicky potion! Mas " << zzivoty << " HP a " << mana << " many.\n";
 
 }
+        else{
+    cout << "Sorry toto neuskutecnim.\n";
+        }
+ Inventar[inventar2].clear();
+ return;
+}
+case 3:{
+cout << "WIP\n";
+return;
+}
+case 4:{
+    cout << "Preskocil jsi svuj turn\n";
+    return;
 }
 }
-void generovaniEnemies(float &zzivoty, float mxzivoty, string Inventar[5], string Klasa, float Utok) {
+}
+}
+
+void generovaniEnemies(float &zzivoty, float mxzivoty, string Inventar[5], string Klasa, float Utok, float &mana, float maxmana) {
     int enemyReturn = rand() % 3 + 1;  // pridano return
     float enemyZivoty[3];
 
     for (int i = 0; i < enemyReturn; i++) {
         enemyZivoty[i] = 5.0f;
     }
+    cout << "Objevilo se " << enemyReturn << " nepritel(e)!";
     while (true) {
         bool vsichniMrtvi = true;
         for (int i = 0; i < enemyReturn; i++) {
@@ -78,7 +163,7 @@ void generovaniEnemies(float &zzivoty, float mxzivoty, string Inventar[5], strin
             cout << "Vsichni nepratele byli porazeni!\n";
             break;
         }
-    soubojSequence(zzivoty, mxzivoty, Inventar, Klasa, Utok, enemyZivoty, enemyReturn);
+    soubojSequence(zzivoty, mxzivoty, Inventar, Klasa, Utok, enemyZivoty, enemyReturn, mana, maxmana);
     for(int i = 0; i < enemyReturn; i++){
             if(enemyZivoty[i] > 0){
                 float poskozeni = static_cast<float>(rand()) / RAND_MAX * 0.9f + 0.1f;
@@ -100,12 +185,12 @@ void generovaniEnemies(float &zzivoty, float mxzivoty, string Inventar[5], strin
 
 // Vesnice: doplneni zivota nebo nakup
 // Odstranen zbytecny parametr penizze, upravena signatura
-void vesniceGenerovani(float &zivotyx, float mxzivotyx, string Inventar[5]) {
+void vesniceGenerovani(float &zivotyx, float mxzivotyx, string Inventar[5], float &mana, float maxmana) {
     int volba, volba2;
 
     do {
         cout << "Vitej ve vesnici!\n";
-        cout << "1 - Doplnit zivoty (zdarma)\n";
+        cout << "1 - Doplnit zivoty a manu (zdarma)\n";
         cout << "2 - Jit do obchodu\n";
         cout << "3 - Odejit\n";
         cin >> volba;
@@ -115,7 +200,8 @@ void vesniceGenerovani(float &zivotyx, float mxzivotyx, string Inventar[5]) {
     switch (volba) {
         case 1:
             zivotyx = mxzivotyx;
-            cout << "Vsechny zivoty byly obnoveny.\n";
+            mana = maxmana;
+            cout << "Vsechny zivoty a mana byly obnoveny.\n";
             break;
 
         case 2:
@@ -124,7 +210,7 @@ void vesniceGenerovani(float &zivotyx, float mxzivotyx, string Inventar[5]) {
                 cout << "2 - Revive: ozivi te kdybys zemrel\n";
                 cout << "3 - Alkohol: +? many\n";
                 cout << "4 - Magicky potion: +? HP a +? many\n";
-                cout << "5 - Nic: nic to nedela, tohle nekupuj pls\n";
+                cout << "5 - Nic\n";
                 cin >> volba2;
             } while (!(volba2 >= 1 && volba2 <= 5));
               // Opraveno: puvodne spatna podminka (volba misto volba2 a chybne znamenko)
@@ -209,7 +295,7 @@ void stromFall(float &zzivoty, string Inventar[5]) {
 }
 
 // Vygeneruje jeden level: kontrola zivota, vesnice, strom
-void generujLevel(int Level, float &Zivoty, string Inventar[5], float mxzivoty, string klasa, float Utok){
+void generujLevel(int Level, float &Zivoty, string Inventar[5], float mxzivoty, string klasa, float Utok, float &mana, float maxmana){
     cout << "Level: " << Level << "\n";
 
     // Kontrola game over
@@ -225,11 +311,11 @@ void generujLevel(int Level, float &Zivoty, string Inventar[5], float mxzivoty, 
 
     // Vesnice na urcitych levelech
     if (Level == 3 || Level == 6 || Level == 9 || Level == 12) {
-        vesniceGenerovani(Zivoty, mxzivoty, Inventar);
+        vesniceGenerovani(Zivoty, mxzivoty, Inventar, mana, maxmana);
         // PUVODNE se volalo bez parametru, opraveno
     }
     if (Level >= 1 && Level <= 15 && !(Level == 3 || Level == 6 || Level == 9 || Level == 12)){
-        generovaniEnemies(Zivoty, mxzivoty, Inventar, klasa, Utok);
+        generovaniEnemies(Zivoty, mxzivoty, Inventar, klasa, Utok, mana, maxmana);
     }
 
     // Strom se muze objevit na kazdem levelu
@@ -277,6 +363,7 @@ int main() {
     float maxzivoty = 0.0f;
     float zivoty = 0.0f;
     float utok = 0.0f;
+    float maxenergie = 0.0f;
     float energie = 0.0f;
 
     // [zivoty, utok, mana] pro kazdou klasu
@@ -307,7 +394,7 @@ int main() {
                 if (decision2 == "Ano") {
                     maxzivoty = klasyy[0][0];
                     utok      = klasyy[0][1];
-                    energie   = klasyy[0][2];
+                    maxenergie = klasyy[0][2];
                     klasa3 = "Paladin";
                     cout << "Vybrali jste si Paladina.\n";
                 }
@@ -322,7 +409,7 @@ int main() {
                 if (decision2 == "Ano") {
                     maxzivoty = klasyy[1][0];
                     utok      = klasyy[1][1];
-                    energie   = klasyy[1][2];
+                    maxenergie   = klasyy[1][2];
                     klasa3 = "Lovec";
                     cout << "Vybrali jste si Lovce.\n";
                 }
@@ -337,7 +424,7 @@ int main() {
                 if (decision2 == "Ano") {
                     maxzivoty = klasyy[2][0];
                     utok      = klasyy[2][1];
-                    energie   = klasyy[2][2];
+                    maxenergie   = klasyy[2][2];
                     klasa3 = "Warlock";
                     cout << "Vybrali jste si Warlocka.\n";
                 }
@@ -352,7 +439,7 @@ int main() {
                 if (decision2 == "Ano") {
                     maxzivoty = klasyy[3][0];
                     utok      = klasyy[3][1];
-                    energie   = klasyy[3][2];
+                    maxenergie   = klasyy[3][2];
                     klasa3 = "Mag";
                     cout << "Vybrali jste si Maga.\n";
                 }
@@ -366,12 +453,13 @@ int main() {
       // Dokud uzivatel nenapise "Ano", pokracuje smycka
 
     // Nastaveni aktualniho zivota po vyberu tridy
+    energie = maxenergie;
     zivoty = maxzivoty;  // doplneno, jinak by zivoty nebyly inicializovane
 // mini boss = level 7, 13
 //boss = level 15
 // Prochazeni Levely 1-15
     for (int level = 1; level <= 15; level++) {
-        generujLevel(level, zivoty, inventar, maxzivoty, klasa3, utok);
+        generujLevel(level, zivoty, inventar, maxzivoty, klasa3, utok, energie, maxenergie);
         cout << "\n";
     }
 
